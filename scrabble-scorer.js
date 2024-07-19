5be4a8ca13f01a455e48ee196baf412d22656623
ZerefRD <juanman.1305@gmail.com>
2024-07-18 20:27:27 -0400

    completed assignment

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -27,7 +27,7 @@ function oldScrabbleScorer(word) {
 }
 
 // your job is to finish writing these functions and variables that we've named //
-// don't change the names or your program won't work as expected. //
+// don't change the names or your program won't work as expected.  //
 
 function initialPrompt() {
    console.log("Let's play some scrabble! \n");
